60ccbd8c2d7b4a9470eb73bc008dc3b930eaba05
shivashis <shivashispadhi@gmail.com>
2017-09-03 02:17:36 +0530

    Assign colors while printing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import textwrap
 import bs4
 from urllib import urlopen
 from bs4 import BeautifulSoup
-#class colors
+#class colors https://gist.github.com/vratiu/9780109
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,8 +16,48 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    CYAN = '\033[96m'
+    PURPLE = '\033[35m'
+    GREY_BACK_WHITE_P = '\033[100m'
+    YELLOW = '\033[93m'
 
 #functions
+def print_modified(sentence, color):
+	sentence_wrapped = textwrap.wrap(sentence, 50, break_long_words=1)
+	length_sr = len(sentence_wrapped)
+	for i in range(length_sr):
+		line_tbp = '' #to_be_printed
+		line_tbp+= '|'
+		line_tbp+= ' '*3
+		line_tbp+= sentence_wrapped[i]
+		line_tbp+= ' '*(55-len(sentence_wrapped[i]))
+		line_tbp+= '|'
+		if color is 'green':
+			print  bcolors.OKGREEN +line_tbp+ bcolors.ENDC 
+		elif color is 'blue':
+			print  bcolors.HEADER +line_tbp+ bcolors.ENDC 
+		elif color is 'red':
+			print  bcolors.FAIL +line_tbp+ bcolors.ENDC 
+		elif color is 'cyan':
+			print  bcolors.CYAN +line_tbp+ bcolors.ENDC 
+		elif color is 'purple':
+			print  bcolors.PURPLE +line_tbp+ bcolors.ENDC 
+		elif color is 'yellow':
+			print  bcolors.YELLOW +line_tbp+ bcolors.ENDC 		
+
+def print_box(index, title, url, author, time, score):
+	#print index,title,url,author,time,score
+	print "+--------+"
+	print bcolors.OKGREEN + "|"+"S.No:"+ str(index) + ' '*(3-len(str(index))) + '|' + bcolors.ENDC
+	print("+--------+-------------------------------------------------+")
+	print_modified(title, 'yellow')
+	print("+----------------------------------------------------------+")
+	print bcolors.CYAN + url + bcolors.ENDC
+	print ''
+	print bcolors.GREY_BACK_WHITE_P + ' Author:' + author + " Score:" + score + " Created:" + time + bcolors.ENDC
+	print "--------------------------------------------------------------------------------"
+
+
 class news:
 	title=''
 	url=''
@@ -27,7 +67,6 @@ def news_handler(start, count):
 	page_indices=[0]
 	page_indices=range(start//30+1,start//30+(start+count)//30+2)
 	print_pages(page_indices,start,start+count)
-stopped = False
 def print_pages(page_indices, start, end):
 	count=0
 	index = 1
@@ -42,9 +81,10 @@ def print_pages(page_indices, start, end):
 			break
 		
 
-
 def print_page(page_index,count,start,end, index):
+	print "Request sent..."
 	html = urlopen(base_url+'/news?p='+str(page_index))
+	print "Recieved, parsing..."
 	bs_obj = BeautifulSoup(html.read(), "lxml")
 	news_chipped = bs_obj.find_all("tr",class_="athing")
 	for i in range(len(news_chipped)):
@@ -66,16 +106,13 @@ def print_page(page_index,count,start,end, index):
 		 	score = dataset2_block.find("span", class_="score").contents[0].split(' ')[0]
 		 	author = dataset2_block.find("a",class_="hnuser").contents[0]
 		 	time = dataset2_block.find("span",class_="age").contents[0].contents[0]
-			print "Index-->"+str(index)
-			print title, link
-		 	print score
-		 	print author, time
+			print_box(index, title, link, author, time, score)
 
 		 	index+=1
 
+		 	#prompt at every 4 items
 		if (index%4 is 0):
-			print "4 waala"
-		 	print "PROMPT:If you want to continue press ENTER else press q+ENTER to exit"
+		 	print bcolors.FAIL + "PROMPT:If you want to continue press ENTER else press q+ENTER to exit" + bcolors.ENDC
 			x = raw_input()
 			if x is 'q':
 				print "Bye!"
@@ -87,31 +124,10 @@ def print_page(page_index,count,start,end, index):
 			break
 
 	return count,index
-	"""
-	html = urlopen(base_url+'/news')
-	bs_obj = BeautifulSoup(html.read(), "lxml")
-	news_chipped = bs_obj.find_all("tr",class_="athing")
-	for i in range(len(news_chipped)):
-		single_news = news_chipped[i]
-		id_single_news = single_news['id']
-		dataset1 = single_news.find_all("td", class_="title")[1].find("a")
-		if(dataset1.has_attr('rel')):
-			continue #Job Advertisements
-		title = dataset1.contents[0]
-		link = dataset1['href']
-		
-		#checking if it is Ask HN
-		if not title.find("Ask HN:"):
-			continue
-		print title, link
-		dataset2_block = single_news.next_sibling.find("td", class_="subtext")
-	 	score = dataset2_block.find("span", class_="score").contents[0].split(' ')[0]
-	 	print score
-	 	author = dataset2_block.find("a",class_="hnuser").contents[0]
-	 	time = dataset2_block.find("span",class_="age").contents[0].contents[0]
-	 	print author, time
-	"""
+
+
 #parser setup
+stopped = False
 base_url = "http://news.ycombinator.com"
 parser = argparse.ArgumentParser()
 parser.add_argument("type_of_dq", type=str,choices=['news','ask','poll']) # dq->data query
