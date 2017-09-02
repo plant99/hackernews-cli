d2027a7d43bb6ba827936b0f16e269a9864ac2d7
shivashis <shivashispadhi@gmail.com>
2017-09-03 02:18:08 +0530

    Test all type of color choices available

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-import textwrap
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    CYAN = '\033[36m'
+    PURPLE = '\033[35m'
 
-def print_modified(sentence):
-	sentence_wrapped = textwrap.wrap(sentence, 50, break_long_words=1)
-	length_sr = len(sentence_wrapped)
-	for i in range(length_sr):
-		line_tbp = '' #to_be_printed
-		line_tbp+= '|'
-		line_tbp+= ' '*3
-		line_tbp+= sentence_wrapped[i]
-		line_tbp+= ' '*(55-len(sentence_wrapped[i]))
-		line_tbp+= '|'
-		print line_tbp
 
-x = "alsdlajksdalksjdlkasjdklajsioasfiyaiufaiuyfiyiyiyoyadiupapiypf9eu[09u09uifdsdifjofiuhsdkfkjsfsdjhfkjsdfhhkjhdfkjshdfsdfkjshdfiuwehisdhfsndfnksydisdhfiusyer hsjdk fsuidfisdhfsduiyf"
-print_modified(x)
+for i in range (200):
+	print '\033['+str(i)+'m' + str(i) + '\033[0m'
