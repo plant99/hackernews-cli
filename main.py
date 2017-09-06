b19acd8b347e33d23bac38967f7af0373786cdef
shivashis <shivashispadhi@gmail.com>
2017-09-06 19:12:26 +0530

    Start building feature to browse comments of an item

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ from bs4 import BeautifulSoup
 import jobs
 import news
 import ask
+import explore
 #parser setup
 stopped = False
 base_url = "http://news.ycombinator.com"
 parser = argparse.ArgumentParser()
-parser.add_argument("type_of_dq", type=str,choices=['news','ask','jobs']) # dq->data query
+parser.add_argument("type_of_dq", type=str,choices=['news','ask','jobs','explore']) # dq->data query
 parser.add_argument("-l", "--limit", type=int, help="number of posts you want to query, MAX_VAL={news:200, ask:50,poll:50}, DEFAULT={[news,ask,poll]:50}") 
 parser.add_argument("-r", "--range", type=str, help="range of posts you want in the output of the form (MIN_INDEX-MAX_INDEX)") 
+parser.add_argument("-c", "--comments_depth", type=int, help="Used with <EXPLORE>, integer input to denote the depth of comments one will go, DEFAULT=1, use -1 to display all available comments") 
+parser.add_argument("-i", "--id", type=int, help="Compulsory for <EXPLORE>, Id of the item, beside the title for each displayed item") 
 args = parser.parse_args()
 
 #variable declaration
@@ -40,3 +43,14 @@ elif dq == 'ask':
 	ask.ask_handler(start, number_of_posts)
 elif dq == 'jobs':
 	jobs.jobs_handler(start, start+number_of_posts)
+elif dq == 'explore':
+	if(args.id):
+		#call function
+		comments_depth = 0
+		if(args.comments_depth):
+			comments_depth = args.comments_depth
+		else:
+			comments_depth = 1
+		explore.explore_handler(args.id, comments_depth)
+	else:
+		print "Pass an integer with -i (or) --id"
