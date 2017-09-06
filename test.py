5777378d43dc76578d6fadea653c80c9f7a76019
shivashis <shivashispadhi@gmail.com>
2017-09-06 19:14:47 +0530

    Test for parsing comments

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 from sys import stdout
 from time import sleep
-for i in range(1,20):
-    print 'asld'
-    stdout.write("\r%d" % i)
-    stdout.flush()
-    sleep(1)
-stdout.write("\n") # move the cursor to the next line
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+html = '<html><body><div>this is cool <p>one</p><p>two</p><p>three<a>anchor</a></p></div></body></html>'
+bs_obj = BeautifulSoup(html, "lxml")
+"""def get_text(bso, string_output):
+	if bso.name is None:
+		string_output+= bso
+	else :
+		for o in bso.contents:
+			try:
+				if bso['class'] is 'reply':
+					continue
+			except:
+				print("Oops!",sys.exc_info()[0],"occured.")
+        		print("Next entry.")
+			string_output = get_text(o, string_output)
+	return string_output
+	"""
+
+
+for i in range(0,56):
+	continue
+	print i
